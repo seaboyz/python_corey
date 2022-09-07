25458a697fb3ae003520ccc8b32610d2e4e088c6
seaboyz <seaboyz@hotmail.com>
2022-09-07 16:24:04 -0400

    Add string_format.py

---
+++ string_format.py
@@ -0,0 +1,34 @@
+# person = {'name': 'Jenn', 'age': 32}
+
+# sentence = "My name is " + person['name'] + ' and I am ' + str(person['age']) + ' years old.'
+
+# sentence = 'My name is {}, and I am {} years old.'.format(person['name'],person['age']
+
+# sentence = f"My name is {person['name']}, and I am {person['age']} years old."
+
+# class Person():
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+# person = Person('john','40')
+
+# sentence = f"My name is {person.name} and I am {person.age} years old."
+
+# sentence = "My name is {name} and I am {age} years old.".format(name='john',age='40')
+
+# for i in range(1,11):
+#     sentence = "The value is {:03}".format(i)
+#     print(sentence)
+
+# from math import pi
+# sentence = 'Pi is equal to {:.2f}'.format(pi)
+
+# sentence= '1 MB is equal to {:,} bytes'.format(1000**2)
+
+import datetime
+my_date = datetime.datetime(2022,9,7,16,11,20)
+
+# print('{:%B %d}'.format(my_date))
+
+sentence = ''
