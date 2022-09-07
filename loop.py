1b1f6076f23eb6d39b6f2ddef46341a8ec547930
seaboyz <seaboyz@hotmail.com>
2022-09-07 15:30:01 -0400

    lopp.py

---
+++ loop.py
@@ -0,0 +1,32 @@
+nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+#     if num == 3:
+#         print("Found!")
+#         break
+#     print(num)
+
+# for num in nums:
+#     if num == 3:
+#         print("Found!")
+#         continue
+#     print(num)
+
+# for num in nums:
+#     if num == 3:
+#         print("Found!")
+#         pass
+#     print(num)
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num,letter)
+
+# for i in range(1, 11):
+#     print(i)
+
+# x = 10
+
+# while x > 0:
+#     print(x)
+#     x -= 1
