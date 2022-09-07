95d430524c94d16b64994e7064613bee08c804b7
seaboyz <seaboyz@hotmail.com>
2022-09-07 09:37:45 -0400

    Add int_float.py

---
+++ int_float.py
@@ -0,0 +1,33 @@
+# num = 3.14
+
+# print(type(num))
+
+# print(3 + 2)
+# print(3 % 5)
+
+# print(3 * (2 + 1))
+
+# num = 1
+# num += 1
+# print(num)
+
+# num = -3
+# print(abs(num))
+
+# num = 3.14
+# print(round(num))
+
+# num_1 = 3
+# num_2 = 2
+
+# print(num_1 == num_2)
+# print(num_1 != num_2)
+
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2)
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
