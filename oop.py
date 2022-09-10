85c04979599daa497edd2c1e141dc4c3fda55433
seaboyz <seaboyz@hotmail.com>
2022-09-10 13:46:18 -0400

    add oop

---
+++ oop.py
@@ -0,0 +1,15 @@
+class Employee:
+    pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = 'Qiang'
+emp_1.last = 'Gao'
+emp_1.email = 'seaboyz@hotmail.com'
+emp_1.pay = 57000
+
+print(emp_1.first)
