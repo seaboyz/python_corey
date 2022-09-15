f37ad0578953e7767b3c2b5c7472feb450ded7ef
seaboyz <seaboyz@hotmail.com>
2022-09-15 14:13:51 -0400

    == vs is

---
+++ equal_vs_is.py
@@ -0,0 +1,30 @@
+# '==' check equality, 'is' check identity(same location in memory)
+
+""" print('coke' == 'pepsi')
+print('coke' == 'coke')
+print('coke' is 'coke') """
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3, 4]
+l3 = [1, 2, 3]
+l4 = l1
+
+""" if l1 == l2:
+    print('Equal')
+else:
+    print('Not Equal')
+
+if l1 is l3:
+    print('Equal')
+else:
+    print('Not Equal')
+
+if l1 is l4:
+    print('Equal')
+else:
+    print('Not Equal')
+ """
+
+""" print(id(l1))
+print(id(l4)) """
+
